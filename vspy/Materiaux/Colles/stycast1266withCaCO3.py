f817e11f51382d68665ca013f466d5facfe5ee03
Valentin SAUVAGE <51372463+ValentinSAUVAGE@users.noreply.github.com>
2020-04-09 11:26:57 +0200

    Add files via upload

---
+++ vspy/Materiaux/Colles/stycast1266withCaCO3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Created on Mon Feb 18 17:25:52 2019 - @author: valentinsauvage """
+
+# Ce programme compile plein de fonctions 
+# Constantes à importer
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+np.set_printoptions(precision=20)
+
+
+def conductivite_thermique(Temperature):
+    
+    """ 
+    
+    Cette fonction permet de determiner la conductivité thermique du matériau
+    
+    Plage de validité : de 0.05 K à 2K
+    Source : "Thermal conductivity of some common cryostat materials between 0.05 and 2 K " from J.R. Olson, 1992
+    
+    Entrée : Temmperature en  K
+    Sortie : La conductivité thermique sera fournie en W/m/K
+     
+    """  
+    
+    A = 330e-4
+    B = 2.65
+    Lambda = A*Temperature**B 
+    return Lambda
